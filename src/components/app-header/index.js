84aabc899898131e8444d1a59d6062e64386cda0
rmb <335147451@qq.com>
2022-08-09 14:30:57 +0800

    顶部导航栏基础样式完成

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,24 +1,32 @@
 import React, { memo, useState } from "react";
 import { NavLink } from "react-router-dom";
+
+import { Input } from "antd";
+
 import { ContentLeft, ContentRight, HeaderWrapper } from "./style";
 import linkMaps from "@/common/links-data.js";
 const RMBAppHeader = memo(() => {
   const [activeName] = useState("nav-active");
   function renderLinks(links) {
-    return links.map((link) =>
-      link.comp === "a" ? (
-        <a href={link.link} key={link.title}>
-          {link.title}
-        </a>
-      ) : (
-        <NavLink
-          to={link.link}
-          key={link.title}
-          className={({ isActive }) => (isActive ? activeName : null)}
-        >
-          {link.title}
-        </NavLink>
-      )
+    return (
+      <ul>
+        {links.map((link) =>
+          link.comp === "a" ? (
+            <li key={link.title}>
+              <a href={link.link}>{link.title}</a>
+            </li>
+          ) : (
+            <li key={link.title}>
+              <NavLink
+                to={link.link}
+                className={({ isActive }) => (isActive ? activeName : null)}
+              >
+                {link.title}
+              </NavLink>
+            </li>
+          )
+        )}
+      </ul>
     );
   }
   return (
@@ -26,11 +34,18 @@ const RMBAppHeader = memo(() => {
       <div className="content wrap-v1">
         <ContentLeft>
           <a href="#/" className="sprite_01 logo">
-            {null}
+            {/* 这里写内容是为了seo优化更容易搜索到，在样式里将文字缩进调足够大就看不到了 */}
+            RMB云音乐
           </a>
           {renderLinks(linkMaps)}
         </ContentLeft>
-        <ContentRight>hhh</ContentRight>
+        <ContentRight>
+          <div className="searchbar-box">
+            <Input placeholder="音乐/视频/电台/用户" className="search"></Input>
+          </div>
+          <div className="center-btn">创作者中心</div>
+          <div className="login-btn">登录</div>
+        </ContentRight>
       </div>
       <div className="divider"></div>
     </HeaderWrapper>
--- src/components/app-header/style.js
+++ src/components/app-header/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import sprite01 from "@/assets/imgs/sprite_01.png";
 
 const HeaderWrapper = styled.div`
   background-color: #242424;
@@ -17,24 +18,94 @@ const HeaderWrapper = styled.div`
 
 const ContentLeft = styled.div`
   display: flex;
-  a:nth-child(n + 2) {
-    padding: 0 19px;
-    color: #ccc;
-    font-size: 14px;
-  }
-  a:nth-child(n + 2):hover {
-    background: #000;
-    color: #fff;
-    text-decoration: none;
-  }
   .logo {
     /* 设置a标签为block才能设置大小 */
     display: block;
     width: 177px;
     height: 69px;
     background-position: 0 0;
+    text-indent: -99999px;
+  }
+  ul {
+    display: flex;
+    li {
+      position: relative;
+      /* 导航li里的每个a标签的样式和hover样式 */
+      a {
+        display: block;
+        height: 100%;
+        padding: 0 19px;
+        color: #ccc;
+        font-size: 14px;
+      }
+      &:hover {
+        a {
+          background: #000;
+          color: #fff;
+          text-decoration: none;
+        }
+      }
+      &:last-child::after {
+        display: block;
+        position: absolute;
+        top: 21px;
+        left: 100px;
+        content: "";
+        width: 28px;
+        height: 19px;
+        background: url(${sprite01}) no-repeat -190px 0;
+      }
+    }
   }
 `;
 
-const ContentRight = styled.div``;
+const ContentRight = styled.div`
+  display: flex;
+  .searchbar-box {
+    position: relative;
+    display: flex;
+    width: 158px;
+    height: 32px;
+    margin-top: 19px;
+    background-color: #fff;
+    border-radius: 32px;
+    background-image: url(${sprite01});
+    background-position: 0 -99px;
+    .search {
+      width: 70%;
+      height: 50%;
+      margin: 8px 0 0 30px;
+    }
+  }
+
+  .center-btn {
+    width: 90px;
+    height: 32px;
+    margin: 19px 0 0 12px;
+    box-sizing: border-box;
+    padding-left: 16px;
+    line-height: 33px;
+    border: 1px solid #4f4f4f;
+    border-radius: 20px;
+    color: #fff;
+    &:hover {
+      cursor: pointer;
+      border-color: #fff;
+    }
+  }
+
+  .login-btn {
+    width: 28px;
+    height: 45px;
+    line-height: 25px;
+    margin: 23px 0 0 20px;
+    padding-right: 22px;
+    color: #787878;
+    &:hover {
+      cursor: pointer;
+      color: #fff;
+      text-decoration: underline;
+    }
+  }
+`;
 export { HeaderWrapper, ContentLeft, ContentRight };
